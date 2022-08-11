88a3d9bb680ff377115856cd066f6b69303f2818
zuijiaoshangyang2 <72388093+zuijiaoshangyang2@users.noreply.github.com>
2022-08-11 15:47:22 +0800

    vision 1

---
+++ nini/js/index.js
@@ -0,0 +1,14 @@
+$('#login-button').click(function (event) {
+	var userName=document.getElementById("userName").value;
+    var pwd=document.getElementById("pwd").value;
+		//修改密码请改此处
+    if(userName=="nini" &&  pwd=="2004.15.8"){
+			event.preventDefault();
+			$('form').fadeOut(500);
+			$('.wrapper').addClass('form-success');
+			setTimeout(function(){location.href="BirthdayCake.html";},2000);
+		}
+	else{
+		alert("Wrong Password");
+	}
+});
